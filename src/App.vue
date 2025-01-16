f17c898b624d8d0327b585eebe9e0b6bba8c06c7
Tudan <tudan@TudandeMacBook-Pro.local>
2025-01-16 10:48:40 +0800

    --

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,18 @@ export default {
   components: {},
   data() {
     return {
+      // expression: '${ name == "张三" && (age >= 18 || sex == "男") && (score > 60 && (grade == "A" || level >= 3)) }',
+      // expression: '${ (salary > 5000 && (department == "IT" || department == "HR")) || (experience >= 5 && (education == "Master" || education == "PhD")) }',
+      // expression: '${ (status == "active" && (age >= 18 && age <= 65)) || (status == "inactive" && (lastLogin > "2023-01-01" || (loginCount > 100 && loginFrequency >= 5))) }',
+      // expression: '${ (age > 18 && (name == "John" || name == "Alice")) && (status == "active" || (lastLogin > "2023-01-01" && loginCount > 100)) }',
+      // expression: '${ age > 18 && name == "John" }',
+      // expression: '${ (age > 18 && (name == "John" || name == "Alice")) || (status == "active" && score >= 80) }',
+      // expression: '${ (salary > 5000 && department == "IT") || (experience >= 5 && education == "Master") }',
+      // expression: '${ (status == "active" && (age >= 18 && age <= 65)) || (status == "inactive" && (lastLogin > "2023-01-01" || loginCount > 100)) }',
+      // expression: '${ (status == "active" && (age >= 18 && (score > 60 || grade == "A"))) || (status == "inactive" && lastLogin > "2023-01-01") }',
+      // expression: '${ ((salary > 5000 && department == "IT") || (experience >= 5 && education == "Master")) && (status == "active" || lastLogin > "2023-01-01") }',
+      // expression: '${ (age > 18 && (name == "John" || name == "Alice")) && (status == "active" || (lastLogin > "2023-01-01" && loginCount > 100)) }',
+      // expression: '${ (status == "active" && (age >= 18 && (score > 60 || grade == "A"))) || (status == "inactive" && (lastLogin > "2023-01-01" || loginCount > 100)) && department in ["IT", "HR"] }',
       // expression: "${ (age > 18 && (name == 'John' || name == 'Alice')) && (status == 'active' || (lastLogin > '2023-01-01' && loginCount > 100)) }",
       expression: "${ serviceTask_ae421a8c05224273a51454d7ddcb121e_abilityId_8cc652606dd44143b3d35808df7f3bc6.resultData.TotalRouteNum == null || serviceTask_ae421a8c05224273a51454d7ddcb121e_abilityId_8cc652606dd44143b3d35808df7f3bc6.resultData.TotalRouteNum == '' }",
       treeOptions: [
